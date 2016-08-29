f7ed3ff98f1c3dc9b25a02c071f67684b57c00ae
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-08-29 10:20:01 +0200

    Finish user signup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
   end
 
   def about
-	end
+  end
 
   def contact
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,7 +19,7 @@ class UserTest < ActiveSupport::TestCase
   test "name should not be too long" do
       @user.name = "a" * 51
       assert_not @user.valid?
-    end
+  end
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
